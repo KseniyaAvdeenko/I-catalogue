495b28a5055c5003304ebb064d2f7c7349f5a1d5
Kseniya Avdeenko <105524305+KseniyaAvdeenko@users.noreply.github.com>
2024-09-12 21:17:52 +0300

    making file uploading

--- src/components/AdminMain/AdminProducts/ProductSettings.tsx
+++ src/components/AdminMain/AdminProducts/ProductSettings.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import styles from "../AdminMain.module.sass";
 import DetailPageSettings from "./DetailPageSettings/DetailPageSettings";
 import ProductsAttributes from "./ProductAttributes/ProductsAttributes";
+import Products from "./Products/Products";
 
 const ProductSettings = React.forwardRef<HTMLElement, {}>(({}, ref) => {
     return (
         <main ref={ref} className={styles.AdminMain}>
             <DetailPageSettings/>
             <ProductsAttributes/>
+            <Products/>
         </main>
     );
 });
---
+++ src/components/AdminMain/AdminProducts/Products/Products.tsx
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import styles from "../../AdminNavbar.module.sass";
+import {useAppSelector} from "../../../../hooks/redux";
+
+const Products = () => {
+    const {prodAttrs} = useAppSelector(state => state.prodAttrsReducer)
+    const {products} = useAppSelector(state => state.productReducer)
+
+    const [files, setFiles] = useState<File[]>([])
+    const [imagePreviews, setImagePreviews] = useState<string[]>([]);
+
+    const onImageChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.files)
+
+        if (e.target.files) {
+            let filesArray = Array.from(e.target.files)
+            setFiles(Array.from(e.target.files))
+            const previews = filesArray.map((file) => URL.createObjectURL(file));
+            setImagePreviews(previews)
+        }
+
+    }
+
+    return (
+        <section id={'addingProdsSection'}
+                 className={[styles.AdminNavbar__container, styles.AdminNavbar__container_margin].join(' ')}>
+            <h3 className={styles.AdminNavbar__subheading}>Добавление товара\услуги</h3>
+            <div className={styles.AdminNavbar__formContainer}>
+                <input type="file" onChange={e => onImageChangeHandler(e)} multiple={true}/>
+
+                <div style={{display: 'flex'}}>
+                    {imagePreviews && imagePreviews.map(image => (
+                        <img src={image} key={image} width={100} height={100} alt=""/>
+                    ))}
+                </div>
+
+            </div>
+        </section>
+    );
+};
+
+export default Products;
